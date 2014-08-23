ed0b4f71a650b8844fcde421723e15f9ac380517
Quarx2k <agent00791@gmail.com>
2014-08-23 22:10:19 +0700

    Do not force build name

--- cm.mk
+++ cm.mk
@@ -7,7 +7,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bootanimation.zip:system/media/bootanimation.zip
 
-CM_BUILDTYPE=AeroRom
-
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := cm_falcon
