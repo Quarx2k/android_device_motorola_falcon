fad6f70687ae30351f6d4ad5594552e8a42bb01e
Quarx2k <agent00791@gmail.com>
2014-08-23 18:48:30 +0700

    Change defconfig name, increase boot/recovery partition sizes, add custom bootanimation

--- cm.mk
+++ cm.mk
@@ -3,5 +3,11 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Override bootanimation
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bootanimation.zip:system/media/bootanimation.zip
+
+CM_BUILDTYPE=AeroRom
+
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := cm_falcon
